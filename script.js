73879fc074b482d2cd93210bd233c4ac9d55a7bf
anabeatrizba <115550114+anabeatrizba@users.noreply.github.com>
2022-12-11 21:29:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let name= "Ana";
+console.log(name);
+let age= 18;
+console.log(age);
+
+let students=[
+    {
+        name: "Maria Josefina"
+        age: 45
+        isStudent: false
+    },
+    {
+        name: "Noivern"
+        age: 11
+        isStudent: true
+    },
+    {
+        name: "Gertrudes"
+        age: 6
+        isStudent: true
+    }
+];
+
+console.log(students.name);
+//Maria Josefina
